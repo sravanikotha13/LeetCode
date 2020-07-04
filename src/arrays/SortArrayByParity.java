bec70b67f36ed047807de9cd5d76a1cfec308d98
sravanik007 <sravanikotha13@gmail.com>
2020-07-04 15:51:06 +0530

    Create SortArrayByParity.java

---
+++ src/arrays/SortArrayByParity.java
@@ -0,0 +1,47 @@
+/******
+Problem Statement:
+
+Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+
+You may return any answer array that satisfies this condition.
+
+ 
+
+Example 1:
+
+Input: [3,1,2,4]
+Output: [2,4,3,1]
+The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+ 
+
+Note:
+
+1 <= A.length <= 5000
+0 <= A[i] <= 5000
+
+
+*****/
+
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+     
+        int i = 0;
+        int j = A.length-1;
+        int temp = 0;
+        
+        while(i<j){
+            if(A[i] %2 > A[j]%2){
+                temp = A[j];
+                A[j] = A[i];
+                A[i] = temp;
+            }
+            
+            if(A[i]%2 == 0)
+                i++;
+            
+            if(A[j]%2 !=0 )
+                j--;
+        }
+        return A;   
+    }
+}
